a729ac8f8ba2ed3bfda50caaa7e42a093beab197
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-05-14 17:09:42 +0300

    Find the largest number

--- lesson2/biggest.py
+++ lesson2/biggest.py
@@ -13,14 +13,14 @@ def biggest(a, b, c):
 print biggest(3, 6, 9)
 #>>> 9
 
-print biggest(6, 9, 3)
+#print biggest(6, 9, 3)
 #>>> 9
 
-print biggest(9, 3, 6)
+#print biggest(9, 3, 6)
 #>>> 9
 
-print biggest(3, 3, 9)
+#print biggest(3, 3, 9)
 #>>> 9
 
-print biggest(9, 3, 9)
+#print biggest(9, 3, 9)
 #>>> 9
