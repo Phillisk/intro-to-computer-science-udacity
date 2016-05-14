95a6243984adfb10eef517995204bb7dfc191b46
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-05-14 17:13:21 +0300

    return true for strings starting with D

--- lesson2/is_friend.py
+++ lesson2/is_friend.py
@@ -38,11 +38,11 @@ def is_friend(name):
     return name[0] == 'D'or name[0] == 'N'
 
 
-print is_friend('Diane')
-#>>> True
+# print is_friend('Diane')
+# #>>> True
 
-print is_friend('Ned')
-#>>> True
+# print is_friend('Ned')
+# #>>> True
 
-print is_friend('Moe')
+# print is_friend('Moe')
 #>>> False
