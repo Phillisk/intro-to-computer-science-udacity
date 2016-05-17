e811d80fbf20f2631aa126c66d252a89ca5fba61
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-05-17 22:19:34 +0300

    rename file

---
+++ lesson1/manipulating_strings.py
@@ -0,0 +1,53 @@
+# Manipulating strings
+
+# using ':' to cut string 
+# Given the variables s and t defined as:
+s = 'udacity'
+t = 'bodacious'
+# write Python code that prints out udacious
+# without using any quote characters in
+# your code.
+
+print s[:3] + t[4:] # result is udacious
+
+# find method
+# Assume text is a variable that
+# holds a string. Write Python code
+# that prints out the position
+# of the first occurrence of 'hoo'
+# in the value of text, or -1 if
+# it does not occur at all.
+
+text = "first hoo" 
+
+
+print text.find('hoo') # result is 6
+
+# find method 2
+# Assume text is a variable that
+# holds a string. Write Python code
+# that prints out the position
+# of the second occurrence of 'zip'
+# in text, or -1 if it does not occur
+# at least twice.
+
+# The Python code should be general enough
+# to pass every possible case where 'zip' 
+# can occur in a string
+
+# Here are two example test cases:
+#text = 'all zip files are zipped' 
+# >>> 18
+# text = 'all zip files are compressed'
+# >>> -1
+
+text = "all zip files are zipped" 
+
+zip1 = text.find('zip')
+
+zip2 = text.find('zip', zip1 +1)
+
+print zip2 # result is 18
+# IMPORTANT BEFORE SUBMITTING: 
+# You should only have one print command in your function
+
